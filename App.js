1c7fe4bf23a1e040ed07a36623ea7b336e6f57eb
first9744 <firstfirst9977@gmail.com>
2022-08-27 02:06:23 +0700

    initial

--- App.js
+++ App.js
@@ -2,9 +2,12 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const doIt = () => {
+    console.log("Hello from console")
+  }
   return (
     <View style={styles.container}>
-      <Text>Hello world</Text>
+      <Text onPress={doIt}>Hello World</Text>
       <StatusBar style="auto" />
     </View>
   );
